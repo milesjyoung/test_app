1fd422f762a557f8bf16e135f6adfc077f602f9e
Miles Young <miles.j.young@gmail.com>
2025-06-06 10:31:23 -0700

    added logging to api gateway

--- infra/backend.tf
+++ infra/backend.tf
@@ -79,6 +79,20 @@ resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw_logs.arn
+    format = jsonencode({
+      requestId       = "$context.requestId",
+      ip              = "$context.identity.sourceIp",
+      requestTime     = "$context.requestTime",
+      httpMethod      = "$context.httpMethod",
+      routeKey        = "$context.routeKey",
+      status          = "$context.status",
+      protocol        = "$context.protocol",
+      responseLength  = "$context.responseLength"
+    })
+  }
 }
 
 resource "aws_lambda_permission" "api_gateway" {
